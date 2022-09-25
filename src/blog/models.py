645c878da19e9efa6456b28d37ae264ab9ce7bf4
Ferando J Soares <biofstec@gmail.com>
2022-09-25 20:11:44 -0300

    finish up crud class

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    autor = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=250)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(default=datetime.now())
+    
